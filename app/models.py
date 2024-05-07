def0fed631058b685b51d331820ac4313b9f2466
armaan <armaanalam65@gmail.com>
2024-05-07 10:33:08 +0530

    add memberes

--- app/models.py
+++ app/models.py
@@ -4,4 +4,7 @@ from django.db import models
 class Student(models.Model):
     name = models.CharField(max_length=100)
     age = models.PositiveIntegerField()
+
+class Members(models.Model):
+    name = models.CharField(max_length=100)
     
